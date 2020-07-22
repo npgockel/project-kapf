183138e4a5806bdbc7a9e96c79654705abc580bb
Forace Avant <themattavant@gmail.com>
2020-07-21 19:08:08 -0700

    fixed user.id persist

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -8,3 +8,4 @@ export { default as LogFood } from "./LogFood";
 export { default as LogInfo } from "./LogInfo";
 export { default as LogSleep } from "./LogSleep";
 export { default as SplashPage } from "./SplashPage";
+export { default as NannyHome } from "./NannyHome";
